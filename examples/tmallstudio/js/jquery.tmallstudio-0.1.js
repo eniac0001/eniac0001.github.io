5da9d8346fb066dd2e00296c700a794e97557a35
china_victory <china_victory@yunshipei.com>
2014-06-13 11:37:51 +0800

    ----

--- examples/tmallstudio/js/jquery.tmallstudio-0.1.js
+++ examples/tmallstudio/js/jquery.tmallstudio-0.1.js
@@ -409,7 +409,7 @@
             }
             
             if (slide !== 'undefined'){
-                scrollPageAndSlide(section, slide);
+                // scrollPageAndSlide(section, slide);
             }else if(destiny.length > 0){
                 scrollPage(destiny);
             }
@@ -646,7 +646,11 @@
         * depending on the given type.
         */
         function isScrolled(type, scrollable){
-
+            if(type === 'top'){
+                return !scrollable.scrollTop();
+            }else if(type === 'bottom'){
+                return scrollable.scrollTop() + scrollable.innerHeight() >= scrollable[0].scrollHeight;
+            }
         }
 
 
